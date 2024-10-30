e87406bd2fa6fc95a7feb18582a61cfd381865c2
Helena Adelsten <helenaltv@gmail.com>
2024-10-30 12:18:19 +0100

    Analytic taggar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Ändra här
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import Favorites from "./pages/Favorites";
 import NavigationMenu from "./components/NavigationMenu";
+import ReactGa from "react-ga4";
+
+const TRACKING_ID = "G-4LK5B4E27J";
+ReactGa.initialize(TRACKING_ID);
 
 const App = () => {
   const [favorites, setFavorites] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGa.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
 
   const addToFavorites = (movie) => {
     const updatedFavorites = [...favorites, movie];
@@ -16,16 +28,14 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <>
       <NavigationMenu />
       <Routes>
-        {" "}
-        {/* Använd Routes istället för Switch */}
         <Route path="/" element={<Home />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import TagManager from "react-gtm-module";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const tagManagerArgs = {
+  gtmId: "GTM-PMZVC96N",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
